e9d79c4914f57da6134111da3b4b4ce4637311a5
sakdag <serhat.akdag@outlook.com>
2021-05-29 12:24:14 +0300

    Search module changed to search among phrases extracted from review instead of review itself. Multiple phrase support and phrase exact match support added.

--- src/search/phrase_search.py
+++ src/search/phrase_search.py
@@ -1,4 +1,5 @@
 import os.path
+from sys import argv
 
 import pandas as pd
 from whoosh.fields import Schema, TEXT, NUMERIC
@@ -13,37 +14,49 @@ def add_documents_to_index(df: pd.DataFrame, ix):
     writer = ix.writer()
 
     for df_index, row in df.iterrows():
-        writer.add_document(review=row['reviewText'], productId=row['asin'])
+        writer.add_document(review=row['reviewText'],
+                            reviewChunks=row['preprocessedReviewChunks'],
+                            productId=row['asin'])
 
     writer.commit()
 
 
-def search_for(ix, query: str):
-    qp = QueryParser("review", schema=ix.schema)
-    q = qp.parse(query)
+def search_for(inverted_index, query_phrases: list):
+    qp = QueryParser("reviewChunks", schema=inverted_index.schema)
+
+    query_as_string = ''
+    for phrase in query_phrases:
+        query_as_string = query_as_string + '"' + phrase + '"'
+
+    q = qp.parse(query_as_string)
 
     with ix.searcher() as searcher:
-        results = searcher.search(q)
-
-        for result in results:
-            print(result)
+        return searcher.search(q)
 
 
 if __name__ == '__main__':
     dirname = os.path.dirname(__file__)
-    preprocessed_file_name = os.path.join(dirname, '../data/', Config.HEADPHONES_REVIEWS_PREPROCESSED_CSV_PATH)
-
-    df = cu.read_dataset(preprocessed_file_name)
-
-    # Create schema
-    schema = Schema(review=TEXT(stored=True), productId=TEXT(stored=True))
-
-    # Create index
-    dirname = os.path.dirname(__file__)
     index_dir = os.path.join(dirname, Config.INDEX_PATH)
 
-    ix = index.create_in(index_dir, schema=schema)
+    if len(argv) == 2 and argv[1] == 'createIndex':
+        preprocessed_file_name = os.path.join(dirname, Config.HEADPHONES_REVIEWS_PREPROCESSED_CSV_PATH)
 
-    add_documents_to_index(df, ix)
+        df = cu.read_dataset(preprocessed_file_name)
 
-    search_for(ix, 'sound quality')
+        # Create schema
+        schema = Schema(review=TEXT(stored=True),
+                        productId=TEXT(stored=True),
+                        reviewChunks=TEXT(stored=True))
+
+        # Create index
+        ix = index.create_in(index_dir, schema=schema)
+        add_documents_to_index(df, ix)
+
+    else:
+        ix = index.open_dir(index_dir)
+
+        phrases_to_query = ['sound quality', 'volume control']
+        results = search_for(ix, phrases_to_query)
+
+        for result in results:
+            print(result)
